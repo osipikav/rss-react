298633d800155c200fa9ac045545662748eb8a35
andrei osipik <osipik.av@yandex.by>
2023-03-28 13:35:48 +0300

    feat: add validation

--- src/components/FormItem/FormItem.tsx
+++ src/components/FormItem/FormItem.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './FormItem.css';
 
-const POSITIONS = [
-  { id: 'fd', value: 'wadawd', title: 'wadawd' },
-  { id: 'fd', value: 'sefsefse', title: 'sefsefse' },
-  { id: 'fd', value: 'pkmnuigi', title: 'pkmnuigi' },
+const DELIVERY = [
+  { id: 'Courier', value: 'Courier delivery', title: 'Courier delivery' },
+  { id: 'Postal', value: 'Postal delivery', title: 'Postal delivery' },
+  { id: 'In-store', value: 'In-store pickup', title: 'In-store pickup' },
 ];
 
 class FormItem extends React.Component {
@@ -13,6 +14,7 @@ class FormItem extends React.Component {
     selectText: '',
     checkboxChecked: false,
     radioChecked: '',
+    isFormSubmitted: false,
   };
 
   nameRef = React.createRef<HTMLInputElement>();
@@ -27,18 +29,54 @@ class FormItem extends React.Component {
       inputName: this.nameRef.current?.value || '',
       inputDate: this.dateRef.current?.value || '',
       selectText: this.selectRef.current?.value || '',
-      checkboxChecked: this.checkboxRef.current?.checked || false,
-      radioChecked: this.radioRefYes.current?.checked ? 'yes' : 'no',
+      checkboxChecked: this.checkboxRef.current?.checked || '',
+      radioChecked: this.radioRefYes.current?.checked ? 'no' : 'yes',
     });
   };
 
-  handleShow = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    console.log('this.state :>> ', this.state);
+  handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log('submit :>> ', this.state);
+    this.setState({ ...this.state, isFormSubmitted: true });
+  };
+
+  noHandleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log('unSubmited :>> ', this.state);
+    this.setState({ ...this.state, isFormSubmitted: true });
+  };
+
+  validateName = (name: string) => {
+    if (!name) {
+      return 'Please enter your name';
+    }
+    if (name.length < 3) {
+      return 'Name must be at least 3 characters long';
+    }
+    return '';
+  };
+
+  validateDate = (date: string) => {
+    const deliveryDate = new Date(date);
+    const currentDate = new Date();
+    if (!date) {
+      return 'Please enter date of delivery';
+    }
+    if (deliveryDate < currentDate) {
+      return 'The date should be in the future';
+    }
+    return '';
   };
 
   render() {
-    const { inputName, inputDate, selectText, checkboxChecked } = this.state;
+    const { inputName, inputDate, selectText, checkboxChecked, radioChecked } = this.state;
+    const isNameValid = this.validateName(inputName.trim()) === '';
+    const isDateValid = this.validateDate(inputDate) === '';
+    const isSelectValid = selectText !== '';
+    const isCheckboxValid = checkboxChecked;
+    const isRadioValid = radioChecked !== '';
+    const isFormValid =
+      isNameValid && isDateValid && isSelectValid && isCheckboxValid && isRadioValid;
 
     return (
       <form>
@@ -50,8 +88,16 @@ class FormItem extends React.Component {
             name="name"
             value={inputName}
             onChange={this.handleChange}
+            className={!isNameValid ? 'error' : ''}
           />
         </label>
+        {/*
+          Отображаем сообщение об ошибке, если имя не заполнено
+          и форма была отправлена (isNameValid === false и isFormSubmitted === true)
+        */}
+        {!isNameValid && this.state.isFormSubmitted && (
+          <div className="error-message">{this.validateName(inputName)}</div>
+        )}
         <br />
         <label>
           Date of delivery:
@@ -61,19 +107,33 @@ class FormItem extends React.Component {
             name="date"
             value={inputDate}
             onChange={this.handleChange}
+            className={!isDateValid ? 'error' : ''}
           />
         </label>
+        {!isDateValid && this.state.isFormSubmitted && (
+          <div className="error-message">{this.validateDate(inputDate)}</div>
+        )}
         <br />
         <label>
           Choose delivery option:
-          <select ref={this.selectRef} value={selectText} onChange={this.handleChange}>
-            {POSITIONS.map(({ id, value, title }) => (
-              <option key={id + value} value={value}>
+          <select
+            ref={this.selectRef}
+            value={selectText}
+            onChange={this.handleChange}
+            className={!isSelectValid ? 'error' : ''}
+          >
+            <option value="">-- Choose an option --</option>
+            {DELIVERY.map(({ id, value, title }) => (
+              <option key={id} value={value}>
                 {title}
               </option>
             ))}
           </select>
         </label>
+
+        {!isSelectValid && this.state.isFormSubmitted && (
+          <div className="error-message">Please choose a delivery option</div>
+        )}
         <br />
         <label>
           I consent to my personal data
@@ -85,6 +145,10 @@ class FormItem extends React.Component {
             onChange={this.handleChange}
           />
         </label>
+
+        {!isCheckboxValid && this.state.isFormSubmitted && (
+          <div className="error-message">Please аgree to the use of personal data</div>
+        )}
         <br />
         <label>
           I want to receive notifications about promo, sales, etc
@@ -94,10 +158,10 @@ class FormItem extends React.Component {
               type="radio"
               name="notificationPreference"
               value="yes"
-              checked={this.state.radioChecked === 'yes'}
+              checked={radioChecked === 'yes'}
               onChange={this.handleChange}
             />
-            <label htmlFor="yes">Да</label>
+            <label htmlFor="yes">Yes</label>
           </div>
           <div>
             <input
@@ -105,17 +169,21 @@ class FormItem extends React.Component {
               type="radio"
               name="notificationPreference"
               value="no"
-              checked={this.state.radioChecked === 'no'}
+              checked={radioChecked === 'no'}
               onChange={this.handleChange}
             />
-            <label htmlFor="no">Нет</label>
+            <label htmlFor="no">No</label>
           </div>
         </label>
-        {/* <label>
-          <input type="file" onChange={this.handleFileInputChange} />
-          <button type="submit">Загрузить</button>
-        </label> */}
-        <button onClick={this.handleShow}>Show</button>
+
+        {!isRadioValid && this.state.isFormSubmitted && (
+          <div className="error-message">Please choose a notification preference</div>
+        )}
+        <br />
+        <button onClick={isFormValid ? this.handleSubmit : this.noHandleSubmit}>
+          {/* <button onClick={this.handleSubmit} disabled={!isFormValid}> */}
+          Submit
+        </button>
       </form>
     );
   }
